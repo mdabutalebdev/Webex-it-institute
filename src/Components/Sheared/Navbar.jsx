8564c3a35b6e356ce23b5b66beeb3144f58d3709
M-JUNAYED <junayedislammd8@gmail.com>
2025-10-07 23:44:41 +0600

    about page add

--- src/Components/Sheared/Navbar.jsx
+++ src/Components/Sheared/Navbar.jsx
@@ -54,6 +54,23 @@ const Navbar = () => {
               We<span className="text-blue-600">bex</span>
             </p>
           </div>
+      {/* Input */}
+          <label className="input">
+            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="Search" />
+          </label>
+
           <div className="  hidden lg:flex">
             <ul className="flex items-center gap-8">
               <li>
